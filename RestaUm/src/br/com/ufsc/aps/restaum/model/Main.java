d4a56ca2f9ed9dbe09609d17de77fec3019b4f1a
LuizMaestri <luizricardomaestri@gmail.com>
2015-07-12 18:42:46 -0300

    main class stand alone

---
+++ RestaUm/src/br/com/ufsc/aps/restaum/model/Main.java
@@ -0,0 +1,13 @@
+package br.com.ufsc.aps.restaum.model;
+
+import br.com.ufsc.aps.restaum.view.TelaJogo;
+
+/**
+ * Created by Luiz Maestri on 12/07/2015.
+ */
+public class Main {
+    public static void main(String[]args)
+    {
+        TelaJogo inicio = new TelaJogo();
+    }
+}
