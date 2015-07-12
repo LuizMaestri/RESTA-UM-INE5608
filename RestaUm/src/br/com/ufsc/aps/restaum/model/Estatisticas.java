bc17c657998b3dbe7840d14e841241b71dde1844
LuizMaestri <luizricardomaestri@gmail.com>
2015-07-12 01:54:47 -0300

    create class Estatisticas

---
+++ RestaUm/src/br/com/ufsc/aps/restaum/model/Estatisticas.java
@@ -0,0 +1,29 @@
+package br.com.ufsc.aps.restaum.model;
+
+public class Estatisticas {
+
+	private Integer qntJogadas;
+	private Integer pecasComidas;
+	private Double tempoJogador;
+	
+	public Integer getQntJogadas() {
+		return qntJogadas;
+	}
+	public void setQntJogadas(Integer qntJogadas) {
+		this.qntJogadas = qntJogadas;
+	}
+	public Integer getPecasComidas() {
+		return pecasComidas;
+	}
+	public void setPecasComidas(Integer pecasComidas) {
+		this.pecasComidas = pecasComidas;
+	}
+	public Double getTempoJogador() {
+		return tempoJogador;
+	}
+	public void setTempoJogador(Double tempoJogador) {
+		this.tempoJogador = tempoJogador;
+	}
+	
+	
+}
