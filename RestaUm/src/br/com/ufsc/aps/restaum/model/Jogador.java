03d91cb596c8a3698bd7459bb4040769bf2a17bc
LuizMaestri <luizricardomaestri@gmail.com>
2015-07-12 01:52:35 -0300

    create class Jogador

---
+++ RestaUm/src/br/com/ufsc/aps/restaum/model/Jogador.java
@@ -0,0 +1,50 @@
+package br.com.ufsc.aps.restaum.model;
+
+public class Jogador {
+
+	private int qntPecas;
+	private boolean vencedor;
+	private boolean jogando;
+	private String nome;
+
+	public Jogador(String nome) {
+		this.nome = nome;
+	}
+
+	public Jogador() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void setQntPecas(int qntPecas) {
+		this.qntPecas = qntPecas;
+	}
+
+	public void setJogando(boolean jogando) {
+		this.jogando = jogando;
+	}
+
+	public boolean isJogando() {
+		return jogando;
+	}
+
+	public int getQntPecas() {
+		return qntPecas;
+	}
+
+	public boolean isVencedor() {
+		return vencedor;
+	}
+
+	public void setVencedor(boolean b) {
+		this.vencedor = b;
+	}
+
+}
